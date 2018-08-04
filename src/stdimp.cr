0eecb9be346443b305c6552f0f134a469eb825a4
Will Lewis <will@wflewis.com>
2018-08-03 20:50:59 -0400

    Update documentation for String#puts

--- src/stdimp.cr
+++ src/stdimp.cr
@@ -1,3 +1,4 @@
+# :nodoc:
 module Stdimp
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 end
--- src/string/puts.cr
+++ src/string/puts.cr
@@ -1,4 +1,14 @@
+#
 class String
+  # Puts itself
+  #
+  # Often I find myself creating a pipeline of tranformations via method chaining, when I'll end up wanting to `puts` the end result.
+  # ```crystal
+  # "hello"
+  #   .upcase
+  #   .reverse
+  #   .puts
+  # ```
   def puts
     puts(self)
   end
